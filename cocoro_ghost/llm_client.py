6a640fd57b6f2aa63db8644ce59851d62b402875
hirona <hiro.98v7@gmail.com>
2026-01-24 13:34:49 +0900

    検索用に会話データを要約したものを別途保持

--- cocoro_ghost/llm_client.py
+++ cocoro_ghost/llm_client.py
@@ -400,6 +400,7 @@ class LlmRequestPurpose:
     ASYNC_STATE_EMBEDDING = "【WORKER】＜＜ 状態埋め込み（state） ＞＞"
     ASYNC_EVENT_AFFECT_EMBEDDING = "【WORKER】＜＜ 感情埋め込み（event_affect） ＞＞"
     ASYNC_WRITE_PLAN = "【WORKER】＜＜ 記憶更新計画作成（WritePlan） ＞＞"
+    ASYNC_EVENT_ASSISTANT_SUMMARY = "【WORKER】＜＜ アシスタント本文要約（events） ＞＞"
 
 
 def _normalize_purpose(purpose: str) -> str:
